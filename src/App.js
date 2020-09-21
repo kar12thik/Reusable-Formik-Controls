68b53dbae41d3737e2aafaae3c603c62318ad442
Karthikeyan Subramanian <46640177+kar12thik@users.noreply.github.com>
2020-09-21 12:17:26 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+import FormikContainer from "./components/FormikContainer";
+
+export default function App() {
+  return (
+    <div className="App">
+      <FormikContainer />
+    </div>
+  );
+}
---
+++ src/components/TextError.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function TextError(props) {
+  // console.log(props.children);
+  return <div className="error">{props.children}</div>;
+}
+
+export default TextError;
