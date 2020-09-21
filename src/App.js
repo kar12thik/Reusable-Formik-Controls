ec758ba361b01f27fd3de380bdefb63231724281
Karthikeyan Subramanian <46640177+kar12thik@users.noreply.github.com>
2020-09-21 21:31:10 +0530

    Registration Form using Formik added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./styles.css";
 import FormikContainer from "./components/FormikContainer";
 import LoginForm from "./components/LoginForm";
+import RegistrationForm from "./components/RegistrationForm";
 
 export default function App() {
   return (
     <div className="App">
       {/* <FormikContainer /> */}
-      <LoginForm />
+      {/* <LoginForm /> */}
+      <RegistrationForm />
     </div>
   );
 }
